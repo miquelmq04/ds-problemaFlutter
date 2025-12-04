01a950a1f42fc13a4efeea61c88bfed432692d9e
cristianAlvarez005 <148985152+cristianAlvarez005@users.noreply.github.com>
2025-12-04 20:34:34 +0100

    Refactor schedule initialization and weekday selection
    Updated initialization of schedule properties to use values from userGroup. Added functionality for selecting weekdays.

--- screen_schedule_group.dart
+++ screen_schedule_group.dart
@@ -22,16 +22,18 @@ class _ScreenScheduleGroupState extends State<ScreenScheduleGroup> {
   final DateFormat timeFormatter = DateFormat('HH:mm');
   late TimeOfDay fromTime;
   late TimeOfDay toTime;
+  late List<int> weekdays;
 
   @override
   void initState() {
     super.initState();
     this.userGroup = widget.userGroup;
     this.now = DateTime.now();
-    this.fromDate = DateTime.now();
-    this.toDate = DateTime.now();
-    this.fromTime = TimeOfDay.now();
-    this.toTime = TimeOfDay.now();
+    this.fromDate = widget.userGroup.schedule.fromDate;
+    this.toDate = widget.userGroup.schedule.toDate;
+    this.fromTime = widget.userGroup.schedule.fromTime;
+    this.toTime = widget.userGroup.schedule.toTime;
+    this.weekdays = widget.userGroup.schedule.weekdays;
   }
 
   Future<void> _selectDate(bool isFromDate) async {
@@ -75,29 +77,27 @@ class _ScreenScheduleGroupState extends State<ScreenScheduleGroup> {
     );
     DateTime pickedHour = DateTime(0,0,0,pickedTime!.hour,pickedTime!.minute);
 
-    if (pickedTime != null) {
-      setState(() {
-        if (isFromTime) {
-          DateTime actualToTime = DateTime(0,0,0,toTime.hour,toTime.minute);
-          if (pickedHour.isAfter(actualToTime) ||
-              pickedHour.isAtSameMomentAs(actualToTime)){
-            _showAlert("Range time",
-                "The From time is after the To time. Please, select a new time range");
-            return;
-          }
-            fromTime = pickedTime;
-        } else {
-          DateTime actualFromTime = DateTime(0,0,0,fromTime.hour,fromTime.minute);
-          if (pickedHour.isBefore(actualFromTime) ||
-              pickedHour.isAtSameMomentAs(actualFromTime)) {
-            _showAlert("Range time",
-                "The To time is before the From time. Please, select a new time range");
-            return;
-          }
-          toTime = pickedTime;
+    setState(() {
+      if (isFromTime) {
+        DateTime actualToTime = DateTime(0,0,0,toTime.hour,toTime.minute);
+        if (pickedHour.isAfter(actualToTime) ||
+            pickedHour.isAtSameMomentAs(actualToTime)){
+          _showAlert("Range time",
+              "The From time is after the To time. Please, select a new time range");
+          return;
         }
-      });
-    }
+        fromTime = pickedTime;
+      } else {
+        DateTime actualFromTime = DateTime(0,0,0,fromTime.hour,fromTime.minute);
+        if (pickedHour.isBefore(actualFromTime) ||
+            pickedHour.isAtSameMomentAs(actualFromTime)) {
+          _showAlert("Range time",
+              "The To time is before the From time. Please, select a new time range");
+          return;
+        }
+        toTime = pickedTime;
+      }
+    });
   }
 
 
@@ -128,14 +128,9 @@ class _ScreenScheduleGroupState extends State<ScreenScheduleGroup> {
     );
   }
 
-
-
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: TheDrawer(context).drawer,
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.primary,
         foregroundColor: Theme.of(context).colorScheme.onPrimary,
@@ -169,9 +164,9 @@ class _ScreenScheduleGroupState extends State<ScreenScheduleGroup> {
                 ),
                 Text(dateFormatter.format(toDate)),
                 IconButton(
-                    onPressed: () => _selectDate(false),
-                    icon: Icon(Icons.calendar_month),
-                    color: Colors.lightBlue,
+                  onPressed: () => _selectDate(false),
+                  icon: Icon(Icons.calendar_month),
+                  color: Colors.lightBlue,
                 )
               ],
             ),
@@ -180,12 +175,29 @@ class _ScreenScheduleGroupState extends State<ScreenScheduleGroup> {
               children: [
                 Text('Weekdays',),
                 SizedBox(
-                  width: 400,
-                  child: WeekdaySelector(onChanged: null,
-                    values: [true,true,true,true,false,false,false],
-                  )
+                    width: 400,
+                    child: WeekdaySelector(
+                      onChanged: (int day) {
+                        setState(() {
+                          int scheduleDay = day + 1;
+                          if (weekdays.contains(scheduleDay)) {
+                            weekdays.remove(scheduleDay);
+                          } else {
+                            weekdays.add(scheduleDay);
+                          }
+                        });
+                      },
+                      values: [
+                        weekdays.contains(1),
+                        weekdays.contains(2),
+                        weekdays.contains(3),
+                        weekdays.contains(4),
+                        weekdays.contains(5),
+                        weekdays.contains(6),
+                        weekdays.contains(7),
+                      ],
+                    )
                 )
-
               ],
             ),
             Row(
@@ -216,7 +228,22 @@ class _ScreenScheduleGroupState extends State<ScreenScheduleGroup> {
                 )
               ],
             ),
-
+            Padding(
+              padding: EdgeInsets.fromLTRB(0, 30, 0, 0),
+              child: ElevatedButton(
+                onPressed: () {
+                  userGroup.schedule.fromDate = fromDate;
+                  userGroup.schedule.toDate = toDate;
+                  userGroup.schedule.fromTime = fromTime;
+                  userGroup.schedule.toTime = toTime;
+                  userGroup.schedule.weekdays = weekdays;
+                  ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(content: Text('Saved'))
+                  );
+                },
+                child: Text("Submit"),
+              ),
+            )
           ],
         ),
       ),
