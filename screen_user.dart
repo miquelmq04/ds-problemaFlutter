8d07c5a04ecdceeb5677d666d113d70c73110622
cristianAlvarez005 <148985152+cristianAlvarez005@users.noreply.github.com>
2025-12-05 11:56:01 +0100

    Refactor ScreenUser layout and spacing adjustments

--- screen_user.dart
+++ screen_user.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:problema/screen_list_users.dart';
-
 import 'data.dart';
-import 'package:problema/InputDecorationTextFormField.dart';
+import 'package:gestio_usuaris/InputDecorationTextFormField.dart';
 
 class ScreenUser extends StatefulWidget {
   late UserGroup userGroup;
@@ -33,41 +31,41 @@ class _ScreenUserState extends State<ScreenUser> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.primary,
-        foregroundColor: Theme.of(context).colorScheme.onPrimary,
-        title: Text("User"),
-      ),
-      body: Form(
-        child: Container(
-          margin: EdgeInsetsGeometry.fromLTRB(30,100,30,0),
-          child: Column(
-            spacing: 40,
-            children: [
-              CircleAvatar(
-                radius: 85,
-                backgroundColor: Color.fromRGBO(240, 240, 240, 1.0),
-                child: CircleAvatar(
-                  backgroundImage: AssetImage(Data.images['new user']!),
-                  radius: 75,
-                ),
-              ),
-              InputDecorationTextFormField(name: 'name', exampleInput: 'new user',controller : _nameController),
-              InputDecorationTextFormField(name: 'credential', exampleInput: '00000', controller : _credentialController),
-              Padding(
-                padding: EdgeInsetsGeometry.fromLTRB(0,30,0,0),
-                child: ElevatedButton(onPressed: () {
-                  String name = _nameController.text;
-                  String credential = _credentialController.text;
-                  User newUser = User(name,credential);
-                  userGroup.users.add(newUser);
-                  ScaffoldMessenger.of(context) .showSnackBar( const SnackBar(content: Text('User created')));
-                  }, child: Text("Submit")),
-              )
-            ],
-          ),
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.primary,
+          foregroundColor: Theme.of(context).colorScheme.onPrimary,
+          title: Text("User"),
         ),
-      )
+        body: Form(
+          child: Container(
+            margin: EdgeInsetsGeometry.fromLTRB(30,50,30,0),
+            child: Column(
+              spacing: 30,
+              children: [
+                CircleAvatar(
+                  radius: 85,
+                  backgroundColor: Color.fromRGBO(240, 240, 240, 1.0),
+                  child: CircleAvatar(
+                    backgroundImage: AssetImage(Data.images['new user']!),
+                    radius: 75,
+                  ),
+                ),
+                InputDecorationTextFormField(name: 'name', exampleInput: 'new user',controller : _nameController),
+                InputDecorationTextFormField(name: 'credential', exampleInput: '00000', controller : _credentialController),
+                Padding(
+                  padding: EdgeInsetsGeometry.fromLTRB(0,20,0,0),
+                  child: ElevatedButton(onPressed: () {
+                    String name = _nameController.text;
+                    String credential = _credentialController.text;
+                    User newUser = User(name,credential);
+                    userGroup.users.add(newUser);
+                    ScaffoldMessenger.of(context) .showSnackBar( const SnackBar(content: Text('User created')));
+                  }, child: Text("Submit")),
+                )
+              ],
+            ),
+          ),
+        )
     );
   }
 }
