b75f1cbd69e7cc462d1eb918020663f48394b176
cristianAlvarez005 <148985152+cristianAlvarez005@users.noreply.github.com>
2025-12-08 14:00:50 +0100

    Implement form validation in ScreenInfoGroup
    Added form validation for name and description fields in ScreenInfoGroup.

--- screen_info_group.dart
+++ screen_info_group.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'data.dart';
-import 'package:problema/InputDecorationTextFormField.dart';
+import 'package:gestio_usuaris/InputDecorationTextFormField.dart';
 
 
 class ScreenInfoGroup extends StatefulWidget {
@@ -13,6 +13,7 @@ class ScreenInfoGroup extends StatefulWidget {
 }
 
 class _ScreenInfoGroupState extends State<ScreenInfoGroup> {
+  final _formKey = GlobalKey<FormState>();
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _descriptionController = TextEditingController();
   late String _title;
@@ -40,25 +41,45 @@ class _ScreenInfoGroupState extends State<ScreenInfoGroup> {
           title: Text("Info ${_title}"),
         ),
         body: Form(
+          key: _formKey,
           child: Container(
             margin: EdgeInsetsGeometry.fromLTRB(30,100,30,0),
             child: Column(
               spacing: 40,
               children: [
                 InputDecorationTextFormField(name: 'name', exampleInput: 'group name',
-                  controller : _nameController,initialValue: userGroup.name,),
+                  controller : _nameController,initialValue: userGroup.name, validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter a group name';
+                    }
+
+                    return null;
+                  },
+              ),
                 InputDecorationTextFormField(name: 'description', exampleInput: 'put your description here',
-                  controller : _descriptionController,minLines: 4,initialValue: userGroup.description,),
+                  controller : _descriptionController,minLines: 4,initialValue: userGroup.description, validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter a description';
+                    }
+
+                    return null;
+
+                  },
+                ),
                 Padding(
                   padding: EdgeInsetsGeometry.fromLTRB(0,30,0,0),
                   child: ElevatedButton(onPressed: () {
-                    userGroup.name = _nameController.text;
-                    userGroup.description = _descriptionController.text;
-                    setState(() {
-                      _title = userGroup.name;
-                    });
-                    ScaffoldMessenger.of(context) .showSnackBar( const SnackBar(content: Text('Group Info Updated')));
-                  }, child: Text("Submit")),
+                    if(_formKey.currentState!.validate()) {
+                      userGroup.name = _nameController.text;
+                      userGroup.description = _descriptionController.text;
+                      setState(() {
+                        _title = userGroup.name;
+                      });
+                      ScaffoldMessenger.of(context) .showSnackBar(
+                          const SnackBar(content: Text('Group Info Updated')));
+                    }
+                  },
+                      child: Text("Submit")),
                 )
               ],
             ),
