6c726495987ae4a058ab0345f322f190bbd47f59
cristianAlvarez005 <148985152+cristianAlvarez005@users.noreply.github.com>
2025-12-10 16:08:24 +0100

    Update import path for screen_list_users.dart

--- screen_group_options.dart
+++ screen_group_options.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:problema/screen_list_users.dart';
+import 'package:gestio_usuaris/screen_list_users.dart';
 
 import 'data.dart';
 import 'screen_actions_group.dart';
@@ -14,29 +14,29 @@ class GroupOptionWidget extends StatelessWidget {
   final VoidCallback? onReturn;
 
   GroupOptionWidget({super.key,required this.title, required this.icon, required this.page,
-  required this.onReturn});
+    required this.onReturn});
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () {
-        selectOption(context);
-      },
-      child : Container(
-      padding: const EdgeInsets.all(8),
-      color: Color.fromRGBO(80, 95, 110,1),
-      alignment: Alignment.center,
-      child : Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [Icon(this.icon,
-          color: Colors.white,
-          size: 100,),Text(this.title,
-            style: TextStyle(
+        onTap: () {
+          selectOption(context);
+        },
+        child : Container(
+          padding: const EdgeInsets.all(8),
+          color: Color.fromRGBO(80, 95, 110,1),
+          alignment: Alignment.center,
+          child : Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [Icon(this.icon,
               color: Colors.white,
-              fontSize: 27,
-            ))],
-      ),
-    )
+              size: 100,),Text(this.title,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 27,
+                ))],
+          ),
+        )
     );
   }
 
@@ -83,19 +83,19 @@ class _ScreenGroupOptionsState extends State<ScreenGroupOptions> {
         title: Text("Group ${userGroup!.name}"),
       ),
       body: GridView.count(
-            primary: false,
-            padding: const EdgeInsets.all(20),
-            crossAxisSpacing: 10,
-            mainAxisSpacing: 10,
-            crossAxisCount: 2,
-            children: <Widget>[
-              GroupOptionWidget(title: 'Info', icon: Icons.description, page: ScreenInfoGroup(userGroup: userGroup),onReturn: _onReturnFromPage,),
-              GroupOptionWidget(title: 'Schedule', icon: Icons.calendar_month, page: ScreenScheduleGroup(userGroup: userGroup),onReturn: _onReturnFromPage),
-              GroupOptionWidget(title: 'Actions', icon: Icons.sensor_door, page: ScreenActionsGroup(userGroup: userGroup),onReturn: _onReturnFromPage),
-              GroupOptionWidget(title: 'Places', icon: Icons.home_work, page: null,onReturn: _onReturnFromPage),
-              GroupOptionWidget(title: 'Users', icon: Icons.manage_accounts, page: ScreenListUsers(userGroup: userGroup),onReturn: _onReturnFromPage),
-            ],
-          ),
+        primary: false,
+        padding: const EdgeInsets.all(20),
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+        crossAxisCount: 2,
+        children: <Widget>[
+          GroupOptionWidget(title: 'Info', icon: Icons.description, page: ScreenInfoGroup(userGroup: userGroup),onReturn: _onReturnFromPage,),
+          GroupOptionWidget(title: 'Schedule', icon: Icons.calendar_month, page: ScreenScheduleGroup(userGroup: userGroup),onReturn: _onReturnFromPage),
+          GroupOptionWidget(title: 'Actions', icon: Icons.sensor_door, page: ScreenActionsGroup(userGroup: userGroup),onReturn: _onReturnFromPage),
+          GroupOptionWidget(title: 'Places', icon: Icons.home_work, page: null,onReturn: _onReturnFromPage),
+          GroupOptionWidget(title: 'Users', icon: Icons.manage_accounts, page: ScreenListUsers(userGroup: userGroup),onReturn: _onReturnFromPage),
+        ],
+      ),
     );
   }
 }
